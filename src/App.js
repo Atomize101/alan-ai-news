b86aff22d1d7939a7ec0d5e9a785f1074310cd21
Chris Patrick <atomize101@gmail.com>
2020-11-13 09:49:29 -0500

    Mapping over news data and displaying card component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
 
 //aa996cbd564644689988152f23adce3d key for newsapi.org
 
 const alanKey = 'b31e2db9646e2432171478312bf270462e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+	const [newsArticles, setNewsArticles] = useState([]);
+
 	useEffect(() => {
 		alanBtn({
 			key: alanKey,
 			onCommand: ({ command, articles }) => {
 				if (command === 'newHeadLines') {
-					console.log(articles);
+					setNewsArticles(articles);
 				}
 			},
 		});
@@ -20,6 +23,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Alan AI news app</h1>
+			<NewsCards articles={newsArticles} />
 		</div>
 	);
 };
