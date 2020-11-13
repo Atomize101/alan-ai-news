235ccd96c1f820ec048aefad4f47aeff05f672e1
Chris Patrick <atomize101@gmail.com>
2020-11-13 11:57:29 -0500

    Images now showing on the cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import useStyles from './styles.js';
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
+	const classes = useStyles();
+
 	return (
 		<Card>
 			<CardActionArea>
 				<CardMedia
+					className={classes.media}
 					image={
 						urlToImage ||
 						'https://media4.s-nbcnews.com/j/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.nbcnews-fp-1200-630.png'
