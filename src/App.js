5c619b15f3f66ee509e6c105fb737d402b987519
Chris Patrick <atomize101@gmail.com>
2020-11-14 08:41:53 -0500

    Logo image and styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 //aa996cbd564644689988152f23adce3d key for newsapi.org
 
 const alanKey = 'b31e2db9646e2432171478312bf270462e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+	const classes = useStyles();
+
 	const [newsArticles, setNewsArticles] = useState([]);
 
 	useEffect(() => {
@@ -22,7 +25,13 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>Alan AI news app</h1>
+			<div className={classes.logoContainer}>
+				<img
+					src="https://alan.app/voice/images/previews/preview.jpg"
+					className={classes.alanLogo}
+					alt="Alan Logo"
+				/>
+			</div>
 			<NewsCards articles={newsArticles} />
 		</div>
 	);
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -9,12 +9,17 @@ export default makeStyles({
 	card: {
 		display: 'flex',
 		flexDirection: 'column',
-		justifyContent: 'center',
+		justifyContent: 'space-between',
 		alignItems: 'center',
 		width: '100%',
 		height: '45vh',
 		padding: '10%',
 		borderRadius: 10,
 		color: 'white',
+	},
+	infoCard: {
+		display: 'flex',
+		flexDirection: 'column',
+		textAlign: 'center',
 	},
 });
